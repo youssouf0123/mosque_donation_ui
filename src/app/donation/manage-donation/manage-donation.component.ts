102546870117454af5c509ed6a0f7e04ea661ca1
Daman <dcamara3@gmail.com>
2025-05-25 12:30:34 -0400

    moved datasource in manage-donations to an external file

--- src/app/donation/manage-donation/manage-donation.component.ts
+++ src/app/donation/manage-donation/manage-donation.component.ts
@@ -5,14 +5,13 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Issue } from 'src/app/model/Issue';
-import { DataSource } from '@angular/cdk/collections';
 
 import { AddDialogComponent } from './dialogs/add/add.dialog.component';
 import { EditDialogComponent } from './dialogs/edit/edit.dialog.component';
 import { DeleteDialogComponent } from './dialogs/delete/delete.dialog.component';
 
-import { BehaviorSubject, fromEvent, merge, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+import { DonationDataSource } from './donation.data.source';
 
 @Component({
   selector: 'manage-donation',
@@ -24,7 +23,7 @@ export class ManageDonationComponent implements OnInit {
   displayedColumns = ['id', 'title', 'state', 'url', 'created_at', 'updated_at', 'actions'];
 
   exampleDatabase: DataService | null;
-  dataSource: ExampleDataSource | null;
+  dataSource: DonationDataSource | null;
   index: number;
   id: number;
 
@@ -122,11 +121,11 @@ export class ManageDonationComponent implements OnInit {
       this.dataSource.filter = this.filter.nativeElement.value;
     }*/
 
-
-
   public loadData() {
-    this.exampleDatabase = new DataService(this.httpClient);
-    this.dataSource = new ExampleDataSource(this.exampleDatabase, this.paginator, this.sort);
+
+    // this.exampleDatabase = new DataService(this.httpClient);
+    this.dataSource = new DonationDataSource(this.dataService, this.paginator, this.sort);
+    
     fromEvent(this.filter.nativeElement, 'keyup')
       // .debounceTime(150)
       // .distinctUntilChanged()
@@ -137,88 +136,4 @@ export class ManageDonationComponent implements OnInit {
         this.dataSource.filter = this.filter.nativeElement.value;
       });
   }
-}
-
-export class ExampleDataSource extends DataSource<Issue> {
-  _filterChange = new BehaviorSubject('');
-
-  get filter(): string {
-    return this._filterChange.value;
-  }
-
-  set filter(filter: string) {
-    this._filterChange.next(filter);
-  }
-
-  filteredData: Issue[] = [];
-  renderedData: Issue[] = [];
-
-  constructor(public _exampleDatabase: DataService,
-    public _paginator: MatPaginator,
-    public _sort: MatSort) {
-    super();
-    // Reset to the first page when the user changes the filter.
-    this._filterChange.subscribe(() => this._paginator.pageIndex = 0);
-  }
-
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Issue[]> {
-    // Listen for any changes in the base data, sorting, filtering, or pagination
-    const displayDataChanges = [
-      this._exampleDatabase.dataChange,
-      this._sort.sortChange,
-      this._filterChange,
-      this._paginator.page
-    ];
-
-    this._exampleDatabase.getAllIssues();
-
-
-    return merge(...displayDataChanges).pipe(map(() => {
-      // Filter data
-      this.filteredData = this._exampleDatabase.data.slice().filter((issue: Issue) => {
-        const searchStr = (issue.id + issue.title + issue.url + issue.created_at).toLowerCase();
-        return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
-      });
-
-      // Sort filtered data
-      const sortedData = this.sortData(this.filteredData.slice());
-
-      // Grab the page's slice of the filtered sorted data.
-      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-      this.renderedData = sortedData.splice(startIndex, this._paginator.pageSize);
-      return this.renderedData;
-    }
-    ));
-  }
-
-  disconnect() { }
-
-
-  /** Returns a sorted copy of the database data. */
-  sortData(data: Issue[]): Issue[] {
-    if (!this._sort.active || this._sort.direction === '') {
-      return data;
-    }
-
-    return data.sort((a, b) => {
-      let propertyA: number | string = '';
-      let propertyB: number | string = '';
-
-      switch (this._sort.active) {
-        case 'id': [propertyA, propertyB] = [a.id, b.id]; break;
-        case 'title': [propertyA, propertyB] = [a.title, b.title]; break;
-        case 'state': [propertyA, propertyB] = [a.state, b.state]; break;
-        case 'url': [propertyA, propertyB] = [a.url, b.url]; break;
-        case 'created_at': [propertyA, propertyB] = [a.created_at, b.created_at]; break;
-        case 'updated_at': [propertyA, propertyB] = [a.updated_at, b.updated_at]; break;
-      }
-
-      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-
-      return (valueA < valueB ? -1 : 1) * (this._sort.direction === 'asc' ? 1 : -1);
-    });
-  }
-
 }
