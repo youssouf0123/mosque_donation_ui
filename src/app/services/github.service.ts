395c038012b0884849f5a091d976730bccf0b86a
Daman <dcamara3@gmail.com>
2025-05-27 16:57:35 -0400

    updated service class for new component

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -24,13 +24,13 @@ export class GithubService {
     perpage: number
   ): Observable<Donation> {
     
-    console.debug('sort column => ' + sort);
+    // console.debug('sort column => ' + sort);
 
     const href = 'https://api.github.com/search/issues';
     const requestUrl = `${this.apiServerUrl}/donation?filter=${filter}+in:title+repo:angular/components&sort=${sort}&order=${order}&page=${page}&size=${perpage}`;
     // const requestUrl = `${this.apiServerUrl}/donation?page=${page}&size=${perpage}&filter=${filter}+in:title+repo:angular/components&sort=${sort}&order=${order}`;
 
-    console.debug(requestUrl);
+    // console.debug(requestUrl);
 
     return this.http.get<Donation>(requestUrl);
   }
